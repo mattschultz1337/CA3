9231d9d68d9a771e2fec99c31107f267ace95b21
Matthew Schultz <mschult7@binghamton.edu>
2018-10-16 15:39:18 -0400

    initial commit

---
+++ LLC.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+#include "LLC.h"
+using namespace std;
+
+  int main(int argc, char *argv[]){
+
+  }
+
+  LLC(const LLC &list){
+    first=list.first;
+    last=list.last;
+  }
---
+++ LLC.h
@@ -0,0 +1,38 @@
+#ifndef LLC_H
+#define LLC_H
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+struct Node {
+ string data;
+ Node *next;
+};
+class LLC {
+    private:
+      Node *first;
+      Node *last;
+    public:
+        int main(int argc, char *argv[]);
+        LLC(){
+          first=NULL;
+          last=NULL;
+        }
+        LLC(const LLC &list);
+        LLC& operator=(const LLC& other);
+        ~LLC();
+        bool contains(const string &);
+        bool insert(const string &);
+        void remove(const string &);
+        void shuffle();
+        LLC& operator+(const LLC& other);
+        void head(int n);
+        string tail();
+        friend ostream& operator<<(ostream& os, const LLC& list);
+        LLC& operator+=(int n);
+        int len();
+        void join(LLC other);
+
+
+
+}
