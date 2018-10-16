3ac204d6f95fa0dc8d59134ae255f7c276121bcd
Matthew Schultz <mschult7@binghamton.edu>
2018-10-16 15:54:58 -0400

    std::

--- LLC.h
+++ LLC.h
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
-
-typedef string;
 struct Node {
- string data;
+ std::string data;
  Node *next;
 };
 class LLC {
@@ -20,13 +18,13 @@ class LLC {
         LLC(const LLC &list);
         LLC& operator=(const LLC& other);
         ~LLC();
-        bool contains(const string &);
-        bool insert(const string &);
-        void remove(const string &);
+        bool contains(const std::string &);
+        bool insert(const std::string &);
+        void remove(const std::string &);
         void shuffle();
         LLC& operator+(const LLC& other);
         void head(int n);
-        string tail();
+        std::string tail();
         friend ostream& operator<<(ostream& os, const LLC& list);
         LLC& operator+=(int n);
         int len();
