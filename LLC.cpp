3289bc39252056f1c22b5e7923ba25b68832878d
Matthew Schultz <mschult7@binghamton.edu>
2018-10-16 21:31:53 -0400

    << op

--- LLC.cpp
+++ LLC.cpp
@@ -2,92 +2,90 @@
 #include <stdlib.h>
 #include "LLC.h"
 using namespace std;
-class LLC{
-  friend ostream& operator<<(ostream& os, const LLC& list);
-  int main(int argc, char *argv[]){
-    string arr [] = {"a","b","c","d","e","f"};
-    LLC* link = new LLC(arr);
-    cout<<"testing: ";
-    cout<<link;
+
+int main(int argc, char *argv[]){
+  string arr [] = {"a","b","c","d","e","f"};
+  LLC* link = new LLC(arr);
+  cout<<"testing: ";
+  cout<<link;
+}
+Node* LLC::newNode(std::string d){
+  Node *n = new Node;
+  n->data = d;
+  n->next=NULL;
+  return n;
+}
+LLC::LLC(string vals[]){
+  for(int i=0;i<sizeof(&vals)/sizeof(vals[0]);i++){
+    insert(vals[i]);
   }
-  Node* LLC::newNode(std::string d){
-    Node *n = new Node;
-    n->data = d;
-    n->next=NULL;
-    return n;
-  }
-  LLC::LLC(string vals[]){
-    for(int i=0;i<sizeof(&vals)/sizeof(vals[0]);i++){
-      insert(vals[i]);
+}
+LLC::LLC(const LLC &list){
+  if(list.first!=NULL){
+    first->data=list.first->data;
+    Node *curr = first;
+    while(curr->next!=NULL){
+      curr=curr->next;
+      insert(curr->data);
     }
-  }
-  LLC::LLC(const LLC &list){
-    if(list.first!=NULL){
-      first->data=list.first->data;
-      Node *curr = first;
-      while(curr->next!=NULL){
-        curr=curr->next;
-        insert(curr->data);
-      }
-    }
+   }
 
-  };
-  LLC& LLC::operator=(const LLC& other){
+};
+LLC& LLC::operator=(const LLC& other){
 
-  }
+}
 
-  bool LLC::insert(const std::string &d){
-    Node *ins = newNode(d);
-    if(first=NULL){
-        first=ins;
+bool LLC::insert(const std::string &d){
+   Node *ins = newNode(d);
+   if(first=NULL){
+      first=ins;
+      last=ins;
+      ins=NULL;
+    } else{
+        last->next = ins;
         last=ins;
-        ins=NULL;
+      }
+   if(last==ins && ins->data==d && ins->next==first){
+      return true;
+   }
+   return false;
+}
+LLC::~LLC(){
+  Node* current = first;
+  while( current != NULL ) {
+    Node* next = current->next;
+    delete current;
+    current = next;
+  }
+  first = NULL;
+}
+bool LLC::contains(const std::string &d){
+  for(Node* curr = first;curr!=NULL;curr = curr->next){
+    if(curr->data==d) return true;
+  }
+  return false;
+}
+void LLC::remove(const std::string &d){
+  Node* last = NULL;
+  for(Node* curr = first;curr!=NULL;curr = curr->next){
+    if(curr->data==d){
+      if(curr==first){
+        first = curr->next;
       } else{
-          last->next = ins;
-          last=ins;
-        }
-        if(last==ins && ins->data==d && ins->next==first){
-          return true;
-        }
-        return false;
+        last->next = curr->next;
       }
-  LLC::~LLC(){
-        Node* current = first;
-        while( current != NULL ) {
-          Node* next = current->next;
-          delete current;
-          current = next;
-        }
-        first = NULL;
-      }
-  bool LLC::contains(const std::string &d){
-        for(Node* curr = first;curr!=NULL;curr = curr->next){
-          if(curr->data==d) return true;
-        }
-        return false;
-      }
-  void LLC::remove(const std::string &d){
-        Node* last = NULL;
-        for(Node* curr = first;curr!=NULL;curr = curr->next){
-          if(curr->data==d){
-            if(curr==first){
-              first = curr->next;
-            } else{
-              last->next = curr->next;
-            }
-          }
-          last = curr;
-        }
-      }
-  void shuffle(){
-
-      }
-  ostream& operator<<(ostream& os, const LLC& list){
-      Node *curr;
-      for(curr = list.first; curr != NULL; curr= curr -> next){
-            os << curr -> data << "\n";
-      }
-
-      return os;
     }
+    last = curr;
+  }
+}
+void shuffle(){
+
+}
+ostream& operator<<(ostream& os, const LLC& list){
+        Node *curr;
+        for(curr =list.first; curr != NULL; curr= curr -> next){
+              os << curr -> data << "\n";
+        }
+
+        return os;
 }
